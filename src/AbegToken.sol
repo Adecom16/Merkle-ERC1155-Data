50f8a93761ce432c48b6443c89e5f7e5b8759710
unknown <adefokunadeoluwaisrael@gmail.com>
2024-04-13 12:29:24 +0100

    updating code

--- src/AbegToken.sol
+++ src/AbegToken.sol
@@ -1,31 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
+
 import "solmate/utils/MerkleProofLib.sol";
-import "solmate/tokens/ERC20.sol";
+import "solmate/tokens/ERC1155.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Merkle is ERC1155 {
+    bytes32 public root;
+    mapping(address => mapping(uint256 => bool)) public hasClaimed;
 
-contract Merkle is ERC20 {
-    bytes32 root;
-
-    constructor(bytes32 _root) ERC20("ABEG", "ABG", 18) {
+    constructor(bytes32 _root) {
         root = _root;
     }
 
-    mapping(address => bool) public hasClaimed;
+    function uri(uint256 id) public view virtual override returns (string memory) {
+        return "";
+    }
 
     function claim(
         address _claimer,
-        uint _amount,
+        uint256 _tokenId,
+        uint256 _amount,
         bytes32[] calldata _proof
     ) external returns (bool success) {
-        require(!hasClaimed[_claimer], "already claimed");
-        bytes32 leaf = keccak256(abi.encodePacked(_claimer, _amount));
+        require(!hasClaimed[_claimer][_tokenId], "already claimed");
+
+        bytes32 leaf = keccak256(abi.encodePacked(_claimer, _tokenId, _amount));
         bool verificationStatus = MerkleProofLib.verify(_proof, root, leaf);
-        require(verificationStatus, "not whitelisted");
-        hasClaimed[_claimer] = true;
-        _mint(_claimer, _amount);
-        success = true;
+        // require(verificationStatus, "not whitelisted");
+
+        hasClaimed[_claimer][_tokenId] = true;
+        _mint(_claimer, _tokenId, _amount, "");
+
+        return true;
     }
 }
